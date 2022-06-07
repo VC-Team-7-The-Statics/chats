c7d5cfa9e4669d8654e04bd2e25e6ac3d1bf22bd
jinwookShin <0201jinwook_@gmail.com>
2022-06-07 17:41:55 +0900

    fefeat: chatlist, chat 컴포넌트 라우팅 설정

---
+++ src/pages/Chat.js
@@ -0,0 +1,12 @@
+import { Title, Button01 } from "@the-statics/shared-components";
+
+function Chatlist() {
+  return (
+    <div className="chat-container">
+      <Title value="채팅" />
+      <Button01 type="button">뒤로</Button01>
+    </div>
+  );
+}
+
+export default Chatlist;
---
+++ src/pages/Chatlist.js
@@ -0,0 +1,11 @@
+import { Title } from "@the-statics/shared-components";
+
+function Chatlist() {
+  return (
+    <div>
+      <Title value="채팅" />
+    </div>
+  );
+}
+
+export default Chatlist;
