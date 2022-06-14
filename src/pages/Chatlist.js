75360c3f10b53c34f3d96e9e5490b4c56332456d
jinwookShin <0201jinwook_@gmail.com>
2022-06-14 22:00:09 +0900

    feat: css style 개정

--- src/pages/Chatlist.js
+++ src/pages/Chatlist.js
@@ -1,4 +1,4 @@
-import { Title, ChatCard } from "@the-statics/shared-components";
+// import { Title, ChatCard } from "@the-statics/shared-components";
 import styles from "../pages/Chatlist.module.scss";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -8,18 +8,25 @@ function Chatlist() {
   const chatroom = useSelector(selectChatroom);
 
   return (
-    <div>
-      <Title value="채팅" />
-      <ul className={styles["chat-list-container"]}>
-        <li className={styles["chat-list"]}>
-          {chatroom.chatrooms.map((chatroom, i) => (
-            <>
+    <div className={styles["chatlist-container"]}>
+      <h1 className={styles.title}>채팅</h1>
+      <ul className={styles["chatlists"]}>
+        {chatroom.chatrooms.map((chatroom, i) => (
+          <>
+            <li key={i} className={styles["chat-list"]}>
               <Link to={`/chat/${chatroom.roomId}`} key={i}>
-                <ChatCard profileImage={chatroom.image} name={chatroom.name} />
+                <div className={styles["chat-card-container"]}>
+                  <div className={styles["image-container"]}>
+                    <img src={chatroom.image} alt="profile" />
+                  </div>
+                  <div className={styles.content}>
+                    <h2 className={styles.name}>{chatroom.name}</h2>
+                  </div>
+                </div>
               </Link>
-            </>
-          ))}
-        </li>
+            </li>
+          </>
+        ))}
       </ul>
     </div>
   );
