5de21d7ed0ed5ce7e3b549224aa8f4ca5506a9df
jinwookShin <0201jinwook_@gmail.com>
2022-06-14 23:10:20 +0900

    feat: 미진행 대화에 대한 메세지 추가

--- src/pages/Chatlist.js
+++ src/pages/Chatlist.js
@@ -10,24 +10,34 @@ function Chatlist() {
   return (
     <div className={styles["chatlist-container"]}>
       <h1 className={styles.title}>채팅</h1>
-      <ul className={styles["chatlists"]}>
-        {chatroom.chatrooms.map((chatroom, i) => (
-          <>
-            <li key={i} className={styles["chat-list"]}>
-              <Link to={`/chat/${chatroom.roomId}`} key={i}>
-                <div className={styles["chat-card-container"]}>
-                  <div className={styles["image-container"]}>
-                    <img src={chatroom.image} alt="profile" />
+      <div className={styles["chatlists-container"]}>
+        {!chatroom.chatrooms.length && (
+          <div className={styles["notification-container"]}>
+            <p className={styles.notification}>
+              진행중인 채팅이 아직 없습니다.
+            </p>
+          </div>
+        )}
+
+        <ul className={styles["chatlists"]}>
+          {chatroom.chatrooms.map((chatroom, i) => (
+            <>
+              <li key={i} className={styles["chat-list"]}>
+                <Link to={`/chat/${chatroom.roomId}`} key={i}>
+                  <div className={styles["chat-card-container"]}>
+                    <div className={styles["image-container"]}>
+                      <img src={chatroom.image} alt="profile" />
+                    </div>
+                    <div className={styles.content}>
+                      <h2 className={styles.name}>{chatroom.name}</h2>
+                    </div>
                   </div>
-                  <div className={styles.content}>
-                    <h2 className={styles.name}>{chatroom.name}</h2>
-                  </div>
-                </div>
-              </Link>
-            </li>
-          </>
-        ))}
-      </ul>
+                </Link>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
